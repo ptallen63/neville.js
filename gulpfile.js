c0df3d927e366a90d815bf3b77acf8148e96e4d7
Paul Allen <ptallen63@gmail.com>
2016-10-13 12:02:30 -0400

    adding the ability to spit our other data of the file

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 /*jshint node: true */
 'use strict';
 
-var gulp = require('gulp');
-var jslint = require('gulp-jslint');
-var jshint = require('gulp-jshint');
-
+const gulp = require('gulp');
+const jslint = require('gulp-jslint');
+const jshint = require('gulp-jshint');
+const istanbul = require('gulp-istanbul');
+const mocha = require('gulp-mocha');
 	
 gulp.task('jshint', function () {
 
@@ -13,7 +14,29 @@ gulp.task('jshint', function () {
 	.pipe(jshint.reporter('jshint-stylish'));
 });
 
+gulp.task('mocha', () => 
+    gulp.src('test.js', {read: false})
+        // gulp-mocha needs filepaths so you can't have any plugins before it
+        .pipe(mocha({reporter: 'nyan'}))
+);
+
+gulp.task('pre-test', function () {
+  return gulp.src(['lib/**/*.js'])
+    // Covering files
+    .pipe(istanbul())
+    // Force `require` to return covered files
+    .pipe(istanbul.hookRequire());
+});
+
+gulp.task('test', ['pre-test'], function () {
+  return gulp.src(['test.js'])
+    .pipe(mocha())
+    // Creating the reports after tests ran
+    .pipe(istanbul.writeReports())
+    // Enforce a coverage of at least 90%
+    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+});
 
 gulp.task('default',['jshint'], function() {
- 
+ 	gulp.watch(['lib/**/*.js','*.js'], ['jshint']);
 });
