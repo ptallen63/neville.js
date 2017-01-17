2ca757225c74ded6a00cd453f7ebfec5d0fe5eba
Paul Allen <ptallen63@gmail.com>
2017-01-17 15:35:04 -0500

    tweaks

--- gulpfile.js
+++ gulpfile.js
@@ -5,37 +5,38 @@ const gulp = require('gulp');
 const jshint = require('gulp-jshint');
 const istanbul = require('gulp-istanbul');
 const mocha = require('gulp-mocha');
-	
-gulp.task('jshint', function () {
 
-	return gulp.src(['*.js', 'lib/**/*.js'])
-	.pipe(jshint())
-	.pipe(jshint.reporter('jshint-stylish'));
+gulp.task('jshint', function() {
+
+    return gulp.src(['*.js', 'lib/**/*.js']).pipe(jshint()).pipe(jshint.reporter('jshint-stylish'));
 });
 
-gulp.task('mocha', () => 
-    gulp.src('test.js', {read: false})
-        // gulp-mocha needs filepaths so you can't have any plugins before it
-        .pipe(mocha({reporter: 'nyan'}))
-);
+gulp.task('mocha', () => gulp.src('test.js', {read: false})
+// gulp-mocha needs filepaths so you can't have any plugins before it
+    .pipe(mocha({reporter: 'nyan'})));
 
-gulp.task('pre-test', function () {
-  return gulp.src(['lib/**/*.js'])
+gulp.task('pre-test', function() {
+    return gulp.src(['lib/**/*.js'])
     // Covering files
-    .pipe(istanbul())
+        .pipe(istanbul())
     // Force `require` to return covered files
-    .pipe(istanbul.hookRequire());
+        .pipe(istanbul.hookRequire());
 });
 
-gulp.task('test', ['pre-test'], function () {
-  return gulp.src(['test/**/*.js'])
-    .pipe(mocha())
+gulp.task('test', ['pre-test'], function() {
+    return gulp.src(['test/**/*.js']).pipe(mocha())
     // Creating the reports after tests ran
-    .pipe(istanbul.writeReports())
+        .pipe(istanbul.writeReports())
     // Enforce a coverage of at least 90%
-    .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } }));
+        .pipe(istanbul.enforceThresholds({
+        thresholds: {
+            global: 90
+        }
+    }));
 });
 
-gulp.task('default',['jshint'], function() {
- 	gulp.watch(['lib/**/*.js','*.js'], ['jshint']);
+gulp.task('default', ['jshint'], function() {
+    gulp.watch([
+        'lib/**/*.js', '*.js'
+    ], ['jshint']);
 });
--- readme.md
+++ readme.md
@@ -26,39 +26,11 @@ When asked for input, you must put in the correct path to your file. Also, your 
 
 ### Output
 
-Three files will be outputed:
+Three files will outputed:
 
 - `cleaned.csv` <-- CSV file of your clean emails
 - `dups.csv` <-- CSV file of duplicate emails
 - `blanks.csv` <-- CSV file of blank emails
 - `invalid.csv` <-- CSV file of emails with invalid format
-
-## LICENSE
-
-MIT
-
-```
-MIT License (MIT)
-
-Copyright (c) 2014 - 2016, Paul Allen
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-```
 
 [intro]: https://s3-us-west-2.amazonaws.com/nevillejs/Oct-07-2016+10-32-33.gif "Intro Screen"
